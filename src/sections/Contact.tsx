9df0a56482e46fd21a40ed085d59e0c6d225dd40
Portfilio <ob042025@gmail.com>
2026-08-04 02:00:41 +0200

    feat: enhance Contact section with detailed content and links

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,5 +1,38 @@
 function Contact() {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact" className="px-6 py-24">
+      <div className="mx-auto max-w-7xl">
+        <p>Kontakt</p>
+
+        <h2>Ta gjerne kontakt</h2>
+
+        <p>
+          Har du lyst til å vite mer om meg eller prosjektene mine, er du
+          velkommen til å ta kontakt.
+        </p>
+
+        <div>
+          <a href="mailto: betinao92@hotmail.com">Send e-post</a>
+
+          <a
+            href="https://www.linkedin.com/in/betina-olsen-9077ab306"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+
+          <a
+            href="https://github.com/Betina9?tab=overview&from=2026-07-01&to=2026-07-15"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
