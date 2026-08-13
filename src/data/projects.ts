d38372926042cb3cd09c1d1226566718d8867656
Portfilio <ob042025@gmail.com>
2026-08-13 15:57:06 +0200

    Refactor code structure for improved readability and maintainability

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,22 +1,41 @@
 import type { Project } from "../types/projects";
+import EaserconImage from "../assets/Easercon.png";
+import CraftsvillaImage from "../assets/Craftsvilla.png";
+import PasswordImage from "../assets/password.png";
 
 export const projects: Project[] = [
   {
     id: 1,
-    title: "Captive Portal Builder",
-    description: "Et adminverktøy for å bygge innloggingsportaler.",
-    technologies: ["React", "TypeScript", "Tailwind CSS"],
-    githubUrl: "https://github.com/...",
-    liveUrl: "DIN_LIVE_URL_HER",
-    image: "/images/portal.png",
+    title: "Eastercon",
+    description:
+      "Dette er en nettside for et convention, Eastercon. Her er det gjort fokus på CSS- animation, Transition og transform.",
+    technologies: ["HTML", "CSS"],
+    githubUrl:
+      "https://github.com/Betina9/Using-Animation--Transition---Transofrm",
+    liveUrl:
+      "https://betina9.github.io/Using-Animation--Transition---Transofrm/",
+    image: EaserconImage,
   },
 
   {
     id: 2,
-    title: "GenderWash",
-    description: "Frontend for administrasjon av navnegjenkjenning.",
-    technologies: ["React", "TypeScript", "Material UI"],
-    githubUrl: "https://github.com/...",
-    image: "/images/genderwash.png",
+    title: "Craftsvilla",
+    description:
+      "Craftsvilla er en nettside for en virksomhet som fokuserer på kreativitet, samhold og avslapning for personer i alle aldre. I dette prosjektet har det hvert fokus på front- end.",
+    technologies: ["HTML", "CSS"],
+    githubUrl: "https://github.com/Betina9/Craftsvilla",
+    liveUrl: "https://betina9.github.io/Craftsvilla/index.html",
+    image: CraftsvillaImage,
+  },
+
+  {
+    id: 3,
+    title: "Password maker",
+    description:
+      "Password maker er en egen side som lager et sikkert og unikt passord til deg for å sikre din/ dine profiler.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    githubUrl: "https://github.com/Betina9/Craftsvilla",
+    liveUrl: "https://betina9.github.io/PasswordMaker/",
+    image: PasswordImage,
   },
 ];
